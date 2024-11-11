329ddca8e3b7f15d20f35549d5004ac93184c120
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2024-11-11 14:53:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var skills = [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Node",
+        "Express",
+        "MongoDB"
+    ];
+
+    return skills;
+}
